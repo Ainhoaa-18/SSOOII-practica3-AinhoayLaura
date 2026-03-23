6399c9181006dc8979bf2267104b7eeda618d333
Ainhoa Sanchez Esteban <Ainhoa.Sanchez5@alu.uclm.es>
2026-03-23 12:40:59 +0100

    cambios

---
+++ Usuario.hpp
@@ -0,0 +1,20 @@
+#ifndef USUARIO_H
+#define USUARIO_H
+
+#include <iostream>
+#include <string>
+
+class Usuario{
+    private:
+    int id;
+    std::string categoria;
+
+    public:
+    Usuario(int id, std::string categoria);
+
+
+
+
+};
+
+#endif
